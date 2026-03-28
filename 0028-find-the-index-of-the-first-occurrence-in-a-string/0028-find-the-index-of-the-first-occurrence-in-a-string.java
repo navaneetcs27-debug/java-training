d3be0a73d2d27c297e49c806d0b2d4dcbadeac4a
Navaneet CS-23411348 IILM GN <navaneet.cs27@iilm.edu>
2026-03-28 23:55:05 +0530

    Time: 1 ms (46.4%), Space: 43.2 MB (28.83%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+        // Edge case
+        if (needle.length() == 0) return 0;
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            
+            // Check substring
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
