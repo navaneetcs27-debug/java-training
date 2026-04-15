6f4cf732107bda7dbe24a45d7fb06ff918765754
Navaneet CS-23411348 IILM GN <navaneet.cs27@iilm.edu>
2026-04-15 22:00:14 +0530

    Time: 2 ms (90.47%), Space: 43.3 MB (82.37%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.java
+++ 0390-elimination-game/0390-elimination-game.java
@@ -1,5 +1,24 @@
 class Solution {
     public int lastRemaining(int n) {
+        int head = 1;
+        int step = 1;
+        int remaining = n;
+        boolean left = true;
         
+        while (remaining > 1) {
+            
+            // Move head when:
+            // - left to right OR
+            // - right to left AND odd remaining
+            if (left || remaining % 2 == 1) {
+                head = head + step;
+            }
+            
+            remaining = remaining / 2;
+            step = step * 2;
+            left = !left;
+        }
+        
+        return head;
     }
 }
