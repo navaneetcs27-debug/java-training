36660fbf06b6bda11795741259650befee52e662
Navaneet CS-23411348 IILM GN <navaneet.cs27@iilm.edu>
2026-04-20 21:04:06 +0530

    Time: 2 ms (99.59%), Space: 45.3 MB (27.12%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // important step
+
+        backtrack(0, nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int start, int[] nums, List<Integer> temp, List<List<Integer>> result) {
+        result.add(new ArrayList<>(temp));
+
+        for (int i = start; i < nums.length; i++) {
+
+            // skip duplicates
+            if (i > start && nums[i] == nums[i - 1]) continue;
+
+            temp.add(nums[i]);
+            backtrack(i + 1, nums, temp, result);
+            temp.remove(temp.size() - 1); // backtrack
+        }
+    }
+}
