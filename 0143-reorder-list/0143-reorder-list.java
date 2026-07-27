f701e75ca27f50096a154d2a4983600c6299c6ff
Navaneet CS-23411348 IILM GN <navaneet.cs27@iilm.edu>
2026-07-27 23:27:14 +0530

    Time: 2 ms (87.03%), Space: 49.2 MB (71.92%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void reorderList(ListNode head) {
+
+        if (head == null || head.next == null)
+            return;
+
+        // Step 1: Find middle
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2: Reverse second half
+        ListNode second = slow.next;
+        slow.next = null;
+
+        ListNode prev = null;
+        while (second != null) {
+            ListNode next = second.next;
+            second.next = prev;
+            prev = second;
+            second = next;
+        }
+
+        // Step 3: Merge two halves
+        ListNode first = head;
+        second = prev;
+
+        while (second != null) {
+            ListNode t1 = first.next;
+            ListNode t2 = second.next;
+
+            first.next = second;
+            second.next = t1;
+
+            first = t1;
+            second = t2;
+        }
+    }
+}
